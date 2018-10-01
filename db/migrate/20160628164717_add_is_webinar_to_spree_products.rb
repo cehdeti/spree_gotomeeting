14fe14b6d75054ba78c073155c47d3eedc2abd9c
Nick Barthelemy <nicholas.barthelemy@gmail.com>
2018-10-01 15:22:53 -0500

    Update migrations for rails 5.2.1

--- db/migrate/20160628164717_add_is_webinar_to_spree_products.rb
+++ db/migrate/20160628164717_add_is_webinar_to_spree_products.rb
@@ -1,4 +1,4 @@
-class AddIsWebinarToSpreeProducts < ActiveRecord::Migration
+class AddIsWebinarToSpreeProducts < ActiveRecord::Migration[4.2]
   def change
     add_column :spree_products, :is_webinar, :boolean
   end
