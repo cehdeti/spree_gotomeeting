f53a1a5c4cc7d84d1a37760fad8bd96c18ba231a
Jake Bell <jbbell@umn.edu>
2020-04-29 16:35:22 -0500

    Fixing issue in vimeo_id column migration

--- db/migrate/20160826141512_add_vimeo_id_to_spree_products.rb
+++ db/migrate/20160826141512_add_vimeo_id_to_spree_products.rb
@@ -1,5 +1,9 @@
 class AddVimeoIdToSpreeProducts < ActiveRecord::Migration[4.2]
-  def change
-    add_column :spree_products, :vimeo_id, :integer
+  def up
+    add_column :spree_products, :vimeo_id, :integer unless ActiveRecord::Base.connection.column_exists?(:spree_products, :vimeo_id)
+  end
+
+  def down
+    remove_column :spree_products, :vimeo_id if ActiveRecord::Base.connection.column_exists?(:spree_products, :vimeo_id)
   end
 end
